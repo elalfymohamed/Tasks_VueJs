a620a6e157a742fa7251b9efe4338255b2ace419
ninja <elalfy.mohamed19@gmail.com>
2021-10-31 01:10:10 +0200

    #tasks

---
+++ src/components/Header/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :style="{ background: color }"
+    class="btn"
+    data-toggle="add task"
+    type="button"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <div>
+      <h1>{{ title }}</h1>
+    </div>
+    <div>
+      <Button text="Add Task" color="green" />
+    </div>
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
